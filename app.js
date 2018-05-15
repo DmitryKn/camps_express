b881f52f0d2d3a305818c7102abba20d8b286a64
Dmitry Knyazev <4dmitry.kn@gmail.com>
2018-05-15 08:55:24 -0400

    0.3 post

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
 
 
-var arr = [
+var arrayData = [
   {name: "Saugeen Springs RV Park", image: "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=350"},
   {name: "Amazing Rocky Park Campground", image: "https://bonvoyagejogja.com/wp-content/uploads/2017/02/IGjogjacamping.jpg"},
-  {name: "Marmora KOA", image: "http://www.millets.co.uk/templates/millets.co.uk/_images/homepage2018/180322/Camping/camping-page_13.jpg"}
+  {name: "Marmora KOA", image: "http://businessidei.com/uploads/posts/2013-05/1368357691_mini-kemping-1.jpg"}
 ]
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+//===ROUTES
 app.get("/", (req, res) => {
   res.render("index")
 })
 app.get("/campgrounds", (req, res) => {
-  res.render("campgrounds", {camps: arr});
+  res.render("campgrounds", {camps: arrayData});
 })
-
+app.get("/campgrounds/new", (req,res) => {
+  res.render('new.ejs')
+})
+//===POST
+app.post("/campgrounds", (req, res) => {
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCamp = {name: name, image: image}
+  arrayData.push(newCamp)
+  res.redirect('/campgrounds');
+})
 app.listen(3000, (req, res) => {
   console.log("Server has started. Port: 3000")
 })
